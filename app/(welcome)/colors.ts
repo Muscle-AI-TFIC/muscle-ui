646c9c098e5803a3ce92a2f5472586883314be7f
TobsBP <tobias.bp2004@gmail.com>
2025-08-31 14:03:17 -0300

    build: setup for welcome page

---
+++ app/(welcome)/colors.ts
@@ -0,0 +1,11 @@
+// (welcomeModal)/colors.ts
+
+export const AppColors = {
+    primary: '#DE7047',    // Laranja - para elementos principais
+    secondary: '#528540',  // Verde escuro - para elementos secundários
+    accent: '#CDDE47',     // Verde limão - para realce
+    light: '#FFDE8D',      // Amarelo - para fundos claros
+    background: '#FFFFFF', // Branco - para fundos
+    blurbackground: 'rgba(0, 0, 0, 0.7)', // deixa escuro o fundo
+    closeButton: '#000000'
+};
