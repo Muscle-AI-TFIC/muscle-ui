19eefea96a109acd4f0bd47bf66080e8a211846c
Tobias <tobias.bp2004@gmail.com>
2025-09-01 16:38:23 -0300

    feat: login started

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,19 +1,50 @@
-import { Button } from '@/src/components/Button/Button';
-import { styleLogin } from '@/src/styles/Login';
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Alert, TouchableOpacity } from "react-native";
+import { Button } from "@/src/components/Button/Button";
+import * as styles from "@/src/styles/Login";
+import * as buttonProps from "@/src/components/Button/Button.styles";
 
-import React from "react";
+export const LoginScreen: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function Login() {
+  const handleLogin = () => {
+    if (!email || !password) {
+      Alert.alert("Erro", "Preencha todos os campos");
+      return;
+    }
+
+    Alert.alert("Sucesso", `Bem-vindo, ${email}`);
+  };
+
   return (
-    <View style={styleLogin.container}>
-      <View style={styleLogin.boxTop}>
-        <Button/>
+    <View className={styles.container}>
+      <Text className={styles.title}>Login</Text>
+
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
+        className={styles.input}
+      />
+
+      <TextInput
+        placeholder="Senha"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        className={styles.input}
+      />
+
+      <View className={styles.buttonContainer}>
+        <Button className="box-decoration-slice bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white" title="Entrar" onPress={handleLogin} />
       </View>
 
-      <View style={styleLogin.boxBot}>
-
-      </View>
+      <TouchableOpacity onPress={() => Alert.alert("Recuperar senha")}>
+        <Text className={styles.forgotPassword}>Esqueceu a senha?</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
