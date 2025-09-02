fc6b3de5d82a2212ee437639024b5f7f95135e4a
TobsBP <tobias.bp2004@gmail.com>
2025-09-02 19:01:53 -0300

    feat: simple function to test the return from the button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { TouchableOpacity, Text, GestureResponderEvent } from 'react-native';
 import { styleButton } from './Button.styles';
 
+export const button_test = () => {
+  return "It works"
+}
+
 export const Button = () => {
   return (
-    <button style={styleButton.button} type="submit">
+    <button style={styleButton.button} type="submit" onClick={button_test}>
       <span>Enviar</span>
     </button>
   );
