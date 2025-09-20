3456378fa83cad458769357a7305ea351f3443c9
GalaxyIgor <igornbelisario@gmail.com>
2025-09-20 16:11:01 -0300

    feat: complete training with height input and IMC calculation

--- app/tabs/trainingInput.tsx
+++ app/tabs/trainingInput.tsx
@@ -9,15 +9,24 @@ import { collection, addDoc } from "firebase/firestore";
 export type TrainingInput = {
   tipoTreino: string; // Exemplo: "musculação", "cardio", "crossfit"
   peso: number;       // Peso do usuário em kg
+  altura: number;    // Altura em m
   idade: number;      // Idade em anos
   objetivo?: string;  // Qual o obj do usuario? "ganhar massa, perder peso e por ai vai"
 };
+
+export function calcularIMC(peso: number, altura: number): number {
+  if (peso <= 0 || altura <= 0) {
+    throw new Error("Peso e altura devem ser maiores que zero");
+  }
+  return +(peso / (altura * altura)).toFixed(2);
+}
 
 // Função que valida se os dados estão corretos
 export function validarInput(input: TrainingInput): boolean {
   if (!input.tipoTreino) return false;
   if (input.peso <= 0) return false;
   if (input.idade <= 0) return false;
+  if (input.altura <= 0) return false;
 
   return true;
 }
@@ -29,12 +38,17 @@ export async function enviarInput(input: TrainingInput): Promise<string> {
   }
 
   try {
+    //Calculando IMC
+    const imc = calcularIMC(input.peso, input.altura);
+
     // "trainings" será o nome da coleção no Firestore
     await addDoc(collection(db, "trainings"), {
       tipoTreino: input.tipoTreino,
       peso: input.peso,
       idade: input.idade,
+      altura: input.altura,
       objetivo: input.objetivo || null,
+      imc: imc,
       criadoEm: new Date(), // marca o horário do envio
     });
 
