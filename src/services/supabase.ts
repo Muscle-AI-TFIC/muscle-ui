18fe1d9b320139f3f92863a92df2636ebe8c7281
Tobias <tobias.bp2004@gmail.com>
2025-10-26 16:16:06 -0300

    fix: removed credentials

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dabczdtbzddoihrjuzut.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYmN6ZHRiemRkb2locmp1enV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MTgxNjQsImV4cCI6MjA3Njk5NDE2NH0.h3lJvTdSi3CqhVZlf28dLM4WftOf1rxOAz97czHHicY';
+const supabaseUrl = process.env.supabaseUrl as string
+const supabaseKey = process.env.SUPABASE_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
