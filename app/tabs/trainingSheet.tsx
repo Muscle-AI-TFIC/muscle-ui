669ab1a09391563d710d32210fb6a71f13392254
Tobias <tobias.bp2004@gmail.com>
2025-11-07 10:22:07 -0300

    feat: setup ready

---
+++ app/tabs/trainingSheet.tsx
@@ -0,0 +1,12 @@
+import { styles } from '@/styles/ToDo';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export default function TrainingSheet(){
+  return(
+    <View>
+      <Text style={styles.subText}>
+        Holaa
+      </Text>
+    </View>
+  )
+}
